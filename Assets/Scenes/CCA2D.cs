d7b55de46851f182ae4187f7802317137faa4851
Kevin <thebeast7991@gmail.com>
2021-08-08 23:42:55 -0400

    color spinner with secondary action

--- Assets/Scenes/CCA2D.cs
+++ Assets/Scenes/CCA2D.cs
@@ -14,14 +14,27 @@ public class CCA2D : MonoBehaviour
     [Range(0, MAX_THRESHOLD)]
     public int threshold = 3;
 
-    const int MAX_STATES = 20;
+    const int MAX_STATES = 24;
     [Range(0, MAX_STATES)]
     public int nstates = 3;
 
     public bool moore;
 
 
-    ///// tw
+    ///// two
+
+    [Header("CCA Secondary Params")]
+    [Range(1, MAX_RANGE)]
+    public int range2 = 1;
+
+    [Range(0, MAX_THRESHOLD)]
+    public int threshold2 = 3;
+
+    [Range(0, MAX_STATES)]
+    public int nstates2 = 3;
+
+    public bool moore2;
+
 
 
     [Header("Setup")]
@@ -34,6 +47,8 @@ public class CCA2D : MonoBehaviour
     [Range(1, 50)]
     public int stepMod = 1;
 
+
+    [Header("Color")]
     [Range(0.0f, 1f)]
     public float hueMax = 1;
 
@@ -73,7 +88,6 @@ public class CCA2D : MonoBehaviour
         Reset();
     }
 
-    //https://github.com/madsbangh/EasyButtons
     [Button]
     private void Reset()
     {
@@ -84,22 +98,6 @@ public class CCA2D : MonoBehaviour
         stepKernel = cs.FindKernel("StepKernel");
 
         GPUResetKernel();
-    }
-
-
-    [Button]
-    public void RandomizeParams()
-    {
-        var rand = new System.Random();
-        range = (int)(rand.NextDouble() * (MAX_RANGE - 1)) + 1;
-        threshold = (int)(rand.NextDouble() * MAX_THRESHOLD - 1) + 1;
-        nstates = (int)(rand.NextDouble() * (MAX_STATES - 2)) + 2;
-        moore = rand.NextDouble() <= 0.5;
-
-        cs.SetInt("range", range);
-        cs.SetInt("threshold", threshold);
-        cs.SetInt("nstates", nstates);
-        cs.SetBool("moore", moore);
     }
 
     [Button]
@@ -147,6 +145,102 @@ public class CCA2D : MonoBehaviour
     }
 
 
+    [Button]
+    public void RandomizeParams()
+    {
+        var rand = new System.Random();
+        range = (int)(rand.NextDouble() * (MAX_RANGE - 1)) + 1;
+        threshold = (int)(rand.NextDouble() * MAX_THRESHOLD - 1) + 1;
+        nstates = (int)(rand.NextDouble() * (MAX_STATES - 2)) + 2;
+        moore = rand.NextDouble() <= 0.5;
+
+        cs.SetInt("range", range);
+        cs.SetInt("threshold", threshold);
+        cs.SetInt("nstates", nstates);
+        cs.SetBool("moore", moore);
+    }
+
+    public void RandomizeSecondaryParams()
+    {
+        var rand = new System.Random();
+        range2 = (int)(rand.NextDouble() * (MAX_RANGE - 1)) + 1;
+        threshold2 = (int)(rand.NextDouble() * MAX_THRESHOLD - 1) + 1;
+        nstates2 = (int)(rand.NextDouble() * (nstates - 2)) + 2;
+        moore2 = rand.NextDouble() <= 0.5;
+
+        cs.SetInt("range", range2);
+        cs.SetInt("threshold", threshold2);
+        cs.SetInt("nstates", nstates2);
+        cs.SetBool("moore", moore2);
+    }
+
+    [Button]
+    public void RandomizeSecondaryParamsWithNoise()
+    {
+        RandomizeSecondaryParams();
+        AddNoise();
+    }
+
+    [Button]
+    public void AddNoise()
+    {
+        int kernel = cs.FindKernel("SecondaryNoiseKernel");
+        cs.SetTexture(kernel, "readTex", readTex);
+        cs.SetTexture(kernel, "writeTex", writeTex);
+        cs.Dispatch(kernel, rez, rez, 1);
+
+        SwapTex();
+    }
+
+    [Button]
+    public void SetPrimaryParams()
+    {
+        cs.SetInt("range", range);
+        cs.SetInt("threshold", threshold);
+        cs.SetInt("nstates", nstates);
+        cs.SetBool("moore", moore);
+    }
+
+    [Button]
+    public void SetSecondaryParams()
+    {
+        cs.SetInt("range", range2);
+        cs.SetInt("threshold", threshold2);
+        cs.SetInt("nstates", nstates2);
+        cs.SetBool("moore", moore2);
+    }
+
+
+    /*
+     * 
+     * 
+     *  STEP 
+     * 
+     * 
+     */
+    [Button]
+    public void Step()
+    {
+        cs.SetTexture(stepKernel, "readTex", readTex);
+        cs.SetTexture(stepKernel, "writeTex", writeTex);
+        cs.SetTexture(stepKernel, "outTex", outTex);
+
+        cs.Dispatch(stepKernel, rez, rez, 1);
+
+        SwapTex();
+
+        outMat.SetTexture("_UnlitColorMap", outTex);
+    }
+
+    ///////////
+
+    private void SwapTex()
+    {
+        RenderTexture tmp = readTex;
+        readTex = writeTex;
+        writeTex = tmp;
+    }
+
     protected RenderTexture CreateTexture(RenderTextureFormat format)
     {
         RenderTexture texture = new RenderTexture(rez, rez, 1, format);
@@ -171,43 +265,6 @@ public class CCA2D : MonoBehaviour
         cs.SetInt("rez", rez);
         cs.Dispatch(k, rez, rez, 1);
         SwapTex();
-    }
-
-    /*
-     * 
-     * 
-     *  STEP 
-     * 
-     * 
-     */
-    [Button]
-    public void Step()
-    {
-        cs.SetTexture(stepKernel, "readTex", readTex);
-        cs.SetTexture(stepKernel, "writeTex", writeTex);
-        cs.SetTexture(stepKernel, "outTex", outTex);
-
-        cs.Dispatch(stepKernel, rez, rez, 1);
-
-        SwapTex();
-
-        outMat.SetTexture("_UnlitColorMap", outTex);
-    }
-
-
-    private void SwapTex()
-    {
-        RenderTexture tmp = readTex;
-        readTex = writeTex;
-        writeTex = tmp;
-    }
-
-        //https://github.com/madsbangh/EasyButtons
-    [Button]
-    private void Test()
-    {
-        Reset();
-        Step();
     }
 
 }
